d1ce31eed6a61eb18642b54e1085b8c405db9a25
Alex <aliniscollins112@gmail.com>
2026-06-03 19:10:06 -0700

    Add rotational matrixes

--- plant.py
+++ plant.py
@@ -8,7 +8,7 @@ class DroneConfig:
     mass: float
     inertia: np.ndarray
     length: float
-    # coefficients for thrust, drug will come later
+    # coefficients for thrust and drag will come later
 
 """
 Rotational Matrix from body to world frame:
@@ -46,12 +46,13 @@ def RK4():
 
 
 class DronePlant:
-    def __init__(self, config, state_vectors):
+    def __init__(self, config, state_vectors, torques):
         self.config = config
         self.position = state_vectors[0:3]
         self.velocity = state_vectors[3:6]
         self.euler_angles = state_vectors[6:9]
         self.omega = state_vectors[9:12]
+        self.torques = torques
 
     def translational_dynamics(self):
         # Derivatives for x, y, z, v_x, v_y, v_z
@@ -59,20 +60,29 @@ class DronePlant:
                    
     def rotational_dynamics(self):
         # Derivatives for roll, pitch, yaw, omega_x, omega_y, omega_z 
-        roll, pitch, yaw = self.euler_angles
+        w_x, w_y, w_z = self.omega
+        phi, theta, psi = self.euler_angles
+
+        euler_angle_matrix = np.array([[1, np.sin(phi)*np.tan(theta), np.cos(phi)*np.tan(theta)],
+                                       [0, np.cos(phi), -np.sin(phi)],
+                                       [0, np.sin(phi)/np.cos(theta), np.cos(phi)/np.cos(theta)]])
         
-        # roll_dot =
-        # pitch_dot = 
-        # yaw_dot = 
+        Ixx = self.config.inertia[0,0]
+        Iyy = self.config.inertia[1,1]
+        Izz = self.config.inertia[2,2]
 
-        omega_x_dot = 
-        omega_y_dot =
-        omega_z_dot = 
+        tau_x, tau_y, tau_z = self.torques
+        
+        phi_dot, theta_dot, psi_dot = euler_angle_matrix @ np.array([w_x, w_y, w_z])
+        w_x_dot = ((Iyy - Izz) * w_y * w_z) / Ixx + tau_x / Ixx
+        w_y_dot = ((Izz - Ixx) * w_x * w_z) / Iyy + tau_y / Iyy
+        w_z_dot = ((Ixx - Iyy) * w_x * w_y) / Izz + tau_z / Izz
 
-        return roll_dot, pitch_dot, yaw_dot, omega_x_dot, omega_y_dot, omega_z_dot
+        return phi_dot, theta_dot, psi_dot, w_x_dot, w_y_dot, w_z_dot
         
 
 
-    def motor_mixing(self);
+    def motor_mixing(self):
         # Torques and thrust will be written here
+        # How would I really write the torques here? 
     
