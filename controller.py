045f8ffb0c628b72bc9f48c13b10efb5771e5030
Alex <aliniscollins112@gmail.com>
2026-06-06 19:25:03 -0700

    Add PID controller

--- controller.py
+++ controller.py
@@ -1,17 +1,32 @@
-class OuterController:
+"""
+PID has
+kp: proportional control
+ki: integral control
+kd: derivative control
+"""
+
+class PID:
     def __init__(self, kp, ki, kd):
         self.kp = kp
         self.ki = ki
         self.kd = kd
 
+        self.integral = 0 # when you integrate over time, this will value will get bigger
+        
+    def update(self):
+        return None
 
-class InnerController:
-    def __init__(self, kp, ki, kd):
-        self.kp = kp
-        self.ki = ki
-        self.kd = kd
+"""
+The CascadedController will have the outer and the inner controller
+The outer controller will output wx, wy and wz, which 
+feed into the inner controller, which in turn outputs
+tau_x, tau_y, tau_z, which feeds directly into 
+plant.py 
+"""
+class CascadedController:
+    def __init__(self):
+        self.inner_controller = 0
 
-    def position_to_thrust
 
 
 
