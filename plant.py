f64c1b2e74822d59d6fae3e4b366d856cc558016
Hoai Phong - Alex <alexhoaiphong@gmail.com>
2026-06-15 11:37:50 -0700

    Update main.py

--- plant.py
+++ plant.py
@@ -3,12 +3,28 @@ from dataclasses import dataclass
 
 g = 9.81 # gravitational acceleration
 
+"""
+Our drone will be an X-configured drone.
+Motor 1      Motor 2
+          |
+          | 
+          |
+Motor 4      Motor 3
+
+Motor 1 and 3 spins clockwise.
+Motor 2 and 4 spin counterclockwise.
+
+"""
+
 @dataclass
 class DroneConfig:
     mass: float
     inertia: np.ndarray
     length: float
     # coefficients for thrust and drag will come later
+    kd: float
+    kt: float # coefficient for tau_roll and tau_pitch
+    kb: float # coefficient for tau_yaw
 
 """
 Rotational Matrix from body to world frame:
@@ -41,93 +57,100 @@ def b2w_rotatation(phi, theta, psi):
 """
 RK4 integration method
 """
-def RK4():
-    return None
+def RK4_step(f, t, y, h):
+    k1 = f(t, y)
+    k2 = f(t + h/2, y + (h/2) * k1)
+    k3 = f(t + h/2, y + (h/2) * k2)
+    k4 = f(t + h, y + h * k3)
+
+    return y + (h / 6) * (k1 + 2*k2 + 2*k3 + k4)
 
 
 class DronePlant:
-    def __init__(self, config, state_vectors, torques):
+    def __init__(self, config, state_vectors):
         self.config = config
         self.position = state_vectors[0:3]
         self.velocity = state_vectors[3:6]
         self.euler_angles = state_vectors[6:9]
-        self.omega = state_vectors[9:12]
-        self.torques = torques
+        self.angular_velocity = state_vectors[9:12]
 
+    # Return torques
+    # def motor_mixing(self):
+    #     # Torques and thrust will be written here
+    #     # How would I really write the torques here? 
+    #     # Should return omega?
+    #     length = self.config.length
+    #     kt = self.config.kt
+    #     kb = self.config.kb
+    #     perpendicular_length = length/np.sqrt(2)
 
-    def translational_dynamics(self, thrust):
-        '''
-        Function's Purpose: Derivatives for x, y, z, v_x, v_y, v_z        
-        Input: 
-            - Force of Body Frame: Thrust, Drag, Gravity
-            - Sensor's Velocity 
-            - Drone's mass
-            - Euler's Angles
-            - g = 9.81
-        
-        Calculate Velocity Derivative: 
-            - Change force of body frame to intertial frame => Feed Euler angles to b2w rotational matrix 
-            
-            - Calculate d(Velocity) using the formula:
-                Ftotal = thrust + drag + grav
-                => m * a = thrust + drag + grav
-                => a = (1 / m) * (thrust + drag + m * g)
-                => v_dot = (1 / m) * (thrust + drag) + g
+    #     tau_roll, tau_pitch, tau_yaw = self.torques
+    #     tau_roll = kt*(-perpendicular_length * omega_1 ** 2 - perpendicular_length * omega_2 ** 2 + perpendicular_length + omega_3 ** 2 + perpendicular_length + omega_4 ** 2)
+    #     tau_pitch = -perpendicular_length * omega_1 ** 2 + perpendicular_length * omega_2 ** 2 - perpendicular_length + omega_3 ** 2 + perpendicular_length + omega_4 ** 2
+    #     tau_yaw = kb( -omega_1**2 + omega_2 ** 2 - omega_3 ** 2 + omega_4 ** 2)
 
-        Calculate the Positional Derivative: 
-            - Update the current velocity using acceleration over a very small time step - t = 0.001
-
-        '''
-        
-        '''
-        Note for the future: 
-            - This code temporarily don't have drag because it has not been calculated 
-            - Future code will have to include drag
-        '''
-        # Grabbing input
-        m = self.config.mass
-        phi, theta, psi = self.euler_angles
-        RM_b2w = b2w_rotatation(phi, theta, psi)
-
-        thrust_world = RM_b2w @ thrust 
-        # drag_world = RM_b2w @ drag
-        gravity = np.matrix([0, 0, g]).T
-        delta_time = 0.001
-
-        # Calculate velocity derivative
-        # velocity_dot = (1.0 / m) * (thrust_world + drag_world) + gravity
-        
-        velocity_dot = (1.0 / m) * thrust_world + gravity
-        position_dot = self.velocity 
+        # return tau_roll, tau_pitch, tau_yaw
     
-        return velocity_dot, position_dot
+    def state_derivatives(self, thrust, torques):
 
-                   
-    def rotational_dynamics(self):
-        # Derivatives for roll, pitch, yaw, omega_x, omega_y, omega_z 
-        w_x, w_y, w_z = self.omega
+        m = self.config.mass # mass of the drone
+ 
+
+        x, y, z = self.position
+        vx, vy, vz = self.velocity
         phi, theta, psi = self.euler_angles
+        wx, wy, wz = self.angular_velocity
+
+        RM_b2w = b2w_rotatation(phi, theta, psi) # Rotation to convert coordinates from body to inertial frame
+
+        thrust_vector = np.array([0, 0, -thrust]).T
+        
+        thrust_world = RM_b2w @ thrust_vector 
+        gravity = np.array([0, 0, g]).T
+        
+        # Instead of saying x_dot, y_dot, vx_dot, etc..., we will use dx, dy, ...
+
+        dx = vx
+        dy = vy
+        dz = vz
+
+        dvelocity = (1.0 / m) * (thrust_world) + gravity
+        dvx, dvy, dvz = dvelocity
+
+        # Rotational dynamics
 
         euler_angle_matrix = np.array([[1, np.sin(phi)*np.tan(theta), np.cos(phi)*np.tan(theta)],
                                        [0, np.cos(phi), -np.sin(phi)],
                                        [0, np.sin(phi)/np.cos(theta), np.cos(phi)/np.cos(theta)]])
-        
+
         Ixx = self.config.inertia[0,0]
         Iyy = self.config.inertia[1,1]
         Izz = self.config.inertia[2,2]
 
-        tau_x, tau_y, tau_z = self.torques
-        
-        phi_dot, theta_dot, psi_dot = euler_angle_matrix @ np.array([w_x, w_y, w_z])
-        w_x_dot = ((Iyy - Izz) * w_y * w_z) / Ixx + tau_x / Ixx
-        w_y_dot = ((Izz - Ixx) * w_x * w_z) / Iyy + tau_y / Iyy
-        w_z_dot = ((Ixx - Iyy) * w_x * w_y) / Izz + tau_z / Izz
-
-        return phi_dot, theta_dot, psi_dot, w_x_dot, w_y_dot, w_z_dot
-        
+        tau_x, tau_y, tau_z = torques
 
 
-    def motor_mixing(self):
-        # Torques and thrust will be written here
-        # How would I really write the torques here? 
-    
+        dphi, dtheta, dpsi = euler_angle_matrix @ np.array([wx, wy, wz])
+
+        dwx = ((Iyy - Izz) * wy * wz) / Ixx + tau_x / Ixx
+        dwy = ((Izz - Ixx) * wx * wz) / Iyy + tau_y / Iyy
+        dwz = ((Ixx - Iyy) * wx * wy) / Izz + tau_z / Izz
+
+        return np.array([dx, dy, dz, 
+                         dvx, dvy, dvz, 
+                         dphi, dtheta, dpsi, 
+                         dwx, dwy, dwz])
+
+
+# dt = 0.01
+
+# t = np.arange(0, 10, dt)
+
+# print(t)
+
+
+
+
+
+
+
